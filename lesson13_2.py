072ba852265f0c5d7456929726fe75060c57ff95
Kyu226 <pent226@hotmail.com>
2023-09-19 14:37:00 +0800

    motyfy

---
+++ lesson13_2.py
@@ -0,0 +1,8 @@
+import student
+stu1=student.get_student('林XX')
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.total())
+print(stu1.average)
+print(stu1)
--- student.py
+++ student.py
@@ -1,3 +1,7 @@
+#student module
+#提供變數,module提供的常數
+#提供class
+#提供function
 class Student:
     def __init__(self,name:str,chinese:int,english:int,math:int) :
         #實體的attribute
@@ -5,13 +9,22 @@ class Student:
         self.chinese = chinese
         self.english = english
         self.math = math
+
     #實體方法method
     def total(self)->int:
         return self.chinese + self.english +self.math
     
     #建立property
+    @property
     def average(self)->float:
         return self.total() / 3.0
     
     def __repr__(self):
         return f'我是student實體,我的name:{self.name}'
+    
+import random
+def get_student(n:str)->Student:
+    ch = random.randint(50,100)
+    en = random.randint(50,100)
+    ma = random.randint(50,100)
+    return Student(name=n,chinese=ch,english=en,math=ma)
